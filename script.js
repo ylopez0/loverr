d92d6ff1acf0be1f4a7d479cc24915b05c0732ac
ylopez0 <159970828+ylopez0@users.noreply.github.com>
2024-02-13 17:29:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! <3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
+    "Pretty please",
     "Don't do this to me :(",
     "You're breaking my heart",
     "I'm gonna cry...",
