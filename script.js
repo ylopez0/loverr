df1352332d0a168d0fc67772aeeed576fdd91fd8
ylopez0 <159970828+ylopez0@users.noreply.github.com>
2024-02-13 17:34:19 -0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! <3";
+  titleElement.innerHTML = "Yayyy!! <3" TE AMO -Yohan;
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
